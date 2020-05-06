b19757f9aeb7f5112b715e24bd91392ed19d1d5c
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-06 22:08:51 +0200

    Spring-boot initializr

---
+++ src/main/java/com/hassanmahmud/hmpetclinic/HmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.hassanmahmud.hmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hassanmahmud/hmpetclinic/HmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hassanmahmud.hmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

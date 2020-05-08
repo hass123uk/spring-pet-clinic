1029030c62f1540afc74da0e8b535b832173db78
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-08 21:02:12 +0200

    Added BaseEntity, ID for models. Closes #10

--- pet-clinic-data/src/main/java/com/hassanmahmud/hmpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/hassanmahmud/hmpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.hassanmahmud.hmpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;

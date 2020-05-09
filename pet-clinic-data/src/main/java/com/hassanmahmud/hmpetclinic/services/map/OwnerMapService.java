f13bf0f4dced08dc29fb8365894762cbc28d9e50
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-10 00:47:27 +0200

    Add to Owners contact info and pets. Closes #36.

--- pet-clinic-data/src/main/java/com/hassanmahmud/hmpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/hassanmahmud/hmpetclinic/services/map/OwnerMapService.java
@@ -2,12 +2,23 @@ package com.hassanmahmud.hmpetclinic.services.map;
 
 import com.hassanmahmud.hmpetclinic.model.Owner;
 import com.hassanmahmud.hmpetclinic.services.OwnerService;
+import com.hassanmahmud.hmpetclinic.services.PetService;
+import com.hassanmahmud.hmpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,6 +36,28 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
+        if (object == null)
+            return null;
+
+        if (object.getPets() == null)
+            return super.save(object);
+
+        object.getPets().forEach(pet -> {
+            if (pet.getPetType() == null)
+                throw new RuntimeException("Pet Type is required");
+
+            //Ensure the pet type saved on the owner has an id.
+            if (pet.getPetType().getId() == null) {
+                var savedPetType = petTypeService.save(pet.getPetType());
+                pet.setPetType(savedPetType);
+            }
+
+            if (pet.getId() == null) {
+                var savedPet = petService.save(pet);
+                pet.setId(savedPet.getId());
+            }
+        });
+
         return super.save(object);
     }
 

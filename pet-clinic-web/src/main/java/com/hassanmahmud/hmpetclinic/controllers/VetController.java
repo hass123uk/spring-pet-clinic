9db9494d9bbe39ac86d91f70e208eda452478102
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-09 18:04:03 +0200

    List all Vets on Vets index page. Closes #17.

--- pet-clinic-web/src/main/java/com/hassanmahmud/hmpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/hassanmahmud/hmpetclinic/controllers/VetController.java
@@ -1,14 +1,28 @@
 package com.hassanmahmud.hmpetclinic.controllers;
 
+import com.hassanmahmud.hmpetclinic.model.Vet;
+import com.hassanmahmud.hmpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
 
         return "vets/index";
     }

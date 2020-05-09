a0afd5ce5733a6dca7ac455b7808a27f0a16d08d
Hassan Mahmud <hass123uk@hotmail.com>
2020-05-10 00:54:24 +0200

    Create Specialty Map Service. Closes #34.

---
+++ pet-clinic-data/src/main/java/com/hassanmahmud/hmpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.hassanmahmud.hmpetclinic.services;
+
+import com.hassanmahmud.hmpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
